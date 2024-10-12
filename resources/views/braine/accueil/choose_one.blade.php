041431fdd1432f00246fabacf3ed2dc22e3bc483
cyriac <atchue2002@gmail.com>
2024-10-12 04:11:07 +0000

    nouvelle modification

---
+++ resources/views/braine/accueil/choose_one.blade.php
@@ -0,0 +1,49 @@
+<section class="choose-one">
+    <div class="auto-container">
+        <div class="inner-container">
+            <!-- Sec Title -->
+            <div class="sec-title light centered">
+                <div class="sec-title_title">why chose us</div>
+                <h2 class="sec-title_heading">Reason to chose our <br> <span>platform</span></h2>
+            </div>
+            <div class="row clearfix">
+
+                <!-- Counter Block One -->
+                <div class="counter-block_one col-lg-4 col-md-6 col-sm-12">
+                    <div class="counter-block_one-inner wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
+                        <div class="counter-block_one-icon fa-brands fa-instagram fa-fw"></div>
+                        <div class="counter-block_one-count"><span class="odometer" data-count="8000"></span><i>+</i>
+                        </div>
+                        <div class="counter-block_one-text">More than 8,000 customers have experimented with
+                            Kafkai</div>
+                    </div>
+                </div>
+
+                <!-- Counter Block One -->
+                <div class="counter-block_one col-lg-4 col-md-6 col-sm-12">
+                    <div class="counter-block_one-inner wow fadeInLeft" data-wow-delay="150ms"
+                        data-wow-duration="1500ms">
+                        <div class="counter-block_one-icon fa-brands fa-instagram fa-fw"></div>
+                        <div class="counter-block_one-count"><span class="odometer" data-count="500000"></span><i>+</i>
+                        </div>
+                        <div class="counter-block_one-text">More than 8,000 customers have experimented with
+                            Kafkai</div>
+                    </div>
+                </div>
+
+                <!-- Counter Block One -->
+                <div class="counter-block_one col-lg-4 col-md-6 col-sm-12">
+                    <div class="counter-block_one-inner wow fadeInLeft" data-wow-delay="300ms"
+                        data-wow-duration="1500ms">
+                        <div class="counter-block_one-icon fa-brands fa-instagram fa-fw"></div>
+                        <div class="counter-block_one-count"><span class="odometer" data-count="200000"></span><i>+</i>
+                        </div>
+                        <div class="counter-block_one-text">More than 8,000 customers have experimented with
+                            Kafkai</div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
