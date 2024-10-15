a337b2d6e3224b7f9ee379bd3783e9be14b55f82
WHITESHARK <mr.younouss79@gmail.com>
2024-10-15 19:52:31 +0000

    modif

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AuthRegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -13,10 +14,30 @@ class AuthController extends Controller
 
     public function store(AuthRegisterRequest $resquest){
         $data = $resquest->validated();
-        dd($data);
+        $image = $resquest->validated("image");
+        if($image && !$image->getError()){
+            $data["image"] = $image->store("user","public");
+        }
+        $data["password"] = \Illuminate\Support\Facades\Hash::make($data["password"]);
+        // dd($data);
+        $user = User::create([
+            "email" => $data["email"],
+            "password" => $data["password"],
+        ]);
+
+        $user->Etudiants()->attach(
+            array_diff_key($data, array_flip(['email', 'password']))
+        );
+
+        return redirect()->route("login")->with("success","Succces de la creation du compte");
+        // dd($data);
     }
 
     public function login(){
         return view("braine.login");
+    }
+
+    public function loginStore(){
+        return redirect()->route("");
     }
 }
